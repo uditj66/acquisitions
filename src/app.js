8f3a10bde83aaf65fd41ea0bc2483c6347d358e4
udit jain <uditj66@gmail.com>
2025-09-18 16:58:03 +0530

    Implement eslint and prettier

--- src/app.js
+++ src/app.js
@@ -1,8 +1,8 @@
-import express from "express";
-import dotenv from "dotenv";
+import express from 'express';
+import dotenv from 'dotenv';
 const app = express();
 dotenv.config();
-app.use("/", (req, res) => {
-  res.status(200).send("Hello from acquisitions");
+app.use('/', (req, res) => {
+  res.status(200).send('Hello from acquisitions');
 });
 export default app;
--- src/server.js
+++ src/server.js
@@ -1,5 +1,5 @@
-import app from "./app.js";
-import dotenv from "dotenv";
+import app from './app.js';
+import dotenv from 'dotenv';
 dotenv.config();
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
