9134406d4965de8e55179cf4ed2f866d59a041ef
udit jain <uditj66@gmail.com>
2025-09-18 16:29:18 +0530

    initial commit

---
+++ src/app.js
@@ -0,0 +1,8 @@
+import express from "express";
+import dotenv from "dotenv";
+const app = express();
+dotenv.config();
+app.use("/", (req, res) => {
+  res.status(200).send("Hello from acquisitions");
+});
+export default app;
