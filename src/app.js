dee2f7172a721aeae150668ecb6b4204165ab1ff
udit jain <uditj66@gmail.com>
2025-09-19 20:25:29 +0530

    Integrate logging with Winston, add middleware for security and CORS, and update environment configuration. Adjusted .env variable naming and modified .gitignore to include .env file. Updated package.json and package-lock.json to include new dependencies: cookie-parser, cors, helmet, morgan, and winston.

--- src/app.js
+++ src/app.js
@@ -1,8 +1,28 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import logger from '#config/logger.js';
+import helmet from 'helmet';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
 const app = express();
+app.use(helmet());
+app.use(cors());
+app.use(cookieParser());
+app.use(express.json());
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+// combining our logging library winston with morgan so that morgan logs can also be used to debug
+app.use(
+  morgan('combined', {
+    stream: { write: message => logger.info(message.trim()) },
+  })
+);
 dotenv.config();
 app.use('/', (req, res) => {
+  logger.info('HELLO from acquistions');
   res.status(200).send('Hello from acquisitions');
 });
 export default app;
